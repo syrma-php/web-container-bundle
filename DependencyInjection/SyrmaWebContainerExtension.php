741df91a7809be1380e0b3edb539ca6d82f89a79
Szijarto Tamas <szijarto.tamas.developer@gmail.com>
2015-07-26 14:03:47 +0200

    refactor and fix lib BC breaks

--- DependencyInjection/SyrmaWebContainerExtension.php
+++ DependencyInjection/SyrmaWebContainerExtension.php
@@ -5,8 +5,11 @@ namespace Syrma\WebContainerBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Syrma\WebContainerBundle\DependencyInjection\Compiler\AddExecutorPass;
 use Syrma\WebContainerBundle\DependencyInjection\Compiler\AddRequestHandlerPass;
 use Syrma\WebContainerBundle\DependencyInjection\Compiler\AddServerPass;
 
@@ -30,6 +33,18 @@ class SyrmaWebContainerExtension extends Extension
 
         $this->registerRequestHandlerConfiguration(
             isset($config['request_handler']) ? $config['request_handler'] : array(),
+            $container,
+            $loader
+        );
+
+        $this->registerExceptionHandlerConfiguration(
+            isset($config['exception_handler']) ? $config['exception_handler'] : array(),
+            $container,
+            $loader
+        );
+
+        $this->registerExecutorConfiguration(
+            isset($config['executor']) ? $config['executor'] : array(),
             $container,
             $loader
         );
@@ -100,5 +115,55 @@ class SyrmaWebContainerExtension extends Extension
                 )
             )
         ;
+    }
+
+    /**
+     * @param array            $config
+     * @param ContainerBuilder $container
+     * @param LoaderInterface  $loader
+     */
+    private function registerExceptionHandlerConfiguration(array $config, ContainerBuilder $container, LoaderInterface $loader)
+    {
+        $loader->load('exception_handler.xml');
+
+        if (isset($config['error_page']['template_path'])) {
+            $container->findDefinition('syrma.web_container.error_page_loader')
+                ->replaceArgument(0, $config['error_page']['template_path']);
+        }
+    }
+
+    /**
+     * @param array            $config
+     * @param ContainerBuilder $container
+     * @param LoaderInterface  $loader
+     */
+    private function registerExecutorConfiguration(array $config, ContainerBuilder $container, LoaderInterface $loader)
+    {
+        $loader->load('executor.xml');
+
+        $defaultExecutor = isset($config['default']) ? $config['default'] : null;
+
+        foreach ($config['executorList'] as $name => $executorConf) {
+            $def = new DefinitionDecorator($executorConf['parent_service']);
+            $def
+                ->replaceArgument(0, $executorConf['server'])
+                ->replaceArgument(1, $executorConf['request_handler'])
+                ->replaceArgument(2, new Reference($executorConf['exception_handler_service']))
+                ->addTag(AddExecutorPass::TAG_NAME, array(
+                    'default' => $name == $defaultExecutor,
+                    'alias' => $name,
+                ))
+            ;
+
+            if (!empty($executorConf['use_factory'])) {
+                $def->setFactoryService(new Reference('syrma.web_container.executor.factory'));
+                $def->setFactoryMethod('create');
+            }
+
+            $container->setDefinition(
+                sprintf('syrma.web_container.executor.dynamic.%s', $name),
+                $def
+            );
+        }
     }
 }
